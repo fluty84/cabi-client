312cbbe97eafbc8a46d6d9d6c8b8457ee06e4d42
Eduardo Gordillo <edu.m.gordillo@gmail.com>
2022-05-13 11:00:13 +0200

    adress => address

--- src/components/restaurantList/RestaurantList.jsx
+++ src/components/restaurantList/RestaurantList.jsx
@@ -1,17 +1,17 @@
 import './RestaurantList.css'
 
-const RestaurantList = ({restaurants}) =>{
+const RestaurantList = ({ restaurants }) => {
 
-    return(
+    return (
         <ul>
-            { restaurants.lenght ? <h3>Restaurant LIST</h3> : <h3>Please create Restaurants</h3>}
+            {restaurants.lenght ? <h3>Restaurant LIST</h3> : <h3>Please create Restaurants</h3>}
             {
                 restaurants?.map((restaurant) => {
                     return (
                         <li key={restaurant._id}>
                             <span>{restaurant.name}:</span>
-                            <span>{restaurant.adress}</span>
-                           <hr />
+                            <span>{restaurant.address}</span>
+                            <hr />
                         </li>
                     )
                 })
